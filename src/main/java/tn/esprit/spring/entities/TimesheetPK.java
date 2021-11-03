a6f9ca894d5362afeda90b0d1300c820fa16b99b
mohamed amine laribi <mohamedamine.laribi@esprit.tn>
2021-11-03 23:05:42 +0000

    commit amine

--- src/main/java/tn/esprit/spring/entities/TimesheetPK.java
+++ src/main/java/tn/esprit/spring/entities/TimesheetPK.java
@@ -51,31 +51,6 @@ public class TimesheetPK implements Serializable {
 		return result;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		TimesheetPK other = (TimesheetPK) obj;
-		if (dateDebut == null) {
-			if (other.dateDebut != null)
-				return false;
-		} else if (!dateDebut.equals(other.dateDebut))
-			return false;
-		if (dateFin == null) {
-			if (other.dateFin != null)
-				return false;
-		} else if (!dateFin.equals(other.dateFin))
-			return false;
-		if (idEmploye != other.idEmploye) {
-			return false;
-		}
-		return true;
-	}
-
 	public void setIdMission(int idMission) {
 		this.idMission = idMission;
 	}
