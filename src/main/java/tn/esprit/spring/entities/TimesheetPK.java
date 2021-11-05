10ed499449f087cccd2dfeb5bc9cfb9ae99aa0f2
mohamed amine laribi <mohamedamine.laribi@esprit.tn>
2021-11-05 13:42:53 +0100

    update gitignore

--- src/main/java/tn/esprit/spring/entities/TimesheetPK.java
+++ src/main/java/tn/esprit/spring/entities/TimesheetPK.java
@@ -36,21 +36,7 @@ public class TimesheetPK implements Serializable {
 		this.dateFin = dateFin;
 	}
 
-	//Pour que hibernate peut comparer deux objets (par exemple : recherche de l'objet dans le persistenceContext), 
-	//Il doit pouvoir comparer les primary key des deux entites
-	//Vu que l'entite a une clé composé, on doit implementer la methode equal.
-	//Utiliser l'IDE pour générer le equal et le hashcode
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((dateDebut == null) ? 0 : dateDebut.hashCode());
-		result = prime * result + ((dateFin == null) ? 0 : dateFin.hashCode());
-		result = prime * result + idEmploye;
-		result = prime * result + idMission;
-		return result;
-	}
-
+	
 	public void setIdMission(int idMission) {
 		this.idMission = idMission;
 	}
